30c4aef67b02d7b484f92016ea5d7db6365ed4f1
ranigarad <152371443+ranigarad@users.noreply.github.com>
2024-02-23 17:25:43 +0530

    Add files via upload

---
+++ JS_isDivisible/checkDivisibility.js
@@ -0,0 +1,5 @@
+const isDivisibleBy10 = number => number % 10 === 0;
+
+console.log(isDivisibleBy10(20));  // true
+console.log(isDivisibleBy10(15));  // false
+console.log(isDivisibleBy10(100)); // true
